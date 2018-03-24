a0100c298941483c86bf72d8e3784b67e5035799
Bradford Muron <bradfordmuron@ool-ad020304.dyn.optonline.net>
2018-03-24 16:13:29 -0400

    centering inputs/placeholders

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/style.css';
+//import './style/style.css';
+import './style/styleTwo.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
